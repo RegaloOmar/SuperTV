486204f9d82cdfad4070ae7c1a98db74cb0428b5
Omar Regalado <regaladomendozaomar@gmail.com>
2026-08-12 10:40:52 -0600

    Initial Commit

---
+++ SuperTV/SuperTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperTVApp.swift
+//  SuperTV
+//
+//  Created by Omar Regalado on 12/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SuperTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuperTVTests/SuperTVTests.swift
@@ -0,0 +1,18 @@
+//
+//  SuperTVTests.swift
+//  SuperTVTests
+//
+//  Created by Omar Regalado on 12/08/26.
+//
+
+import Testing
+
+struct SuperTVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
